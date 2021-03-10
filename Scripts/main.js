dbab5c4cb3a3da195cd552d130d978f21ba588f4
ashleyeeee <78965455+ashleyeeee@users.noreply.github.com>
2021-03-10 08:23:40 -0500

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,24 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Project 1 Coming Soon!";
+function buttonone() {
+    document.getElementById("button1").innerHTML = "Project 1 Coming Soon!";
   }
+function buttontwo() {
+    document.getElementById("button2").innerHTML = "Project 2 Coming Soon!"
+}
+function buttonthree() {
+    document.getElementById("button3").innerHTML = "Project 3 Coming Soon!"
+}
+function buttonfour() {
+    document.getElementById("button4").innerHTML = "Project 4 Coming Soon!"
+}
+function buttonfive() {
+    document.getElementById("button5").innerHTML = "Project 5 Coming Soon!"
+}
+function buttonsix() {
+    document.getElementById("button6").innerHTML = "Project 6 Coming Soon!"
+}
+function buttonseven() {
+    document.getElementById("button7").innerHTML = "Project 7 Coming Soon!"
+}
+function buttoneight() {
+    document.getElementById("button8").innerHTML = "Project 8 Coming Soon!"
+}
