49090c9cd6146548d36f5366f6ddd6c6497fd590
ashleyeeee <78965455+ashleyeeee@users.noreply.github.com>
2021-03-21 02:59:58 -0400

    Create main.js

---
+++ pages/Scripts/main.js
@@ -0,0 +1,27 @@
+function buttonone() {
+    alert("Project 1 Coming Soon!");
+  }
+function buttontwo() {
+    alert("Project 2 Coming Soon!");
+}
+function buttonthree() {
+    alert("Project 3 Coming Soon!");
+}    
+function buttonfour() {
+    alert("Project 4 Coming Soon!");
+}
+function buttonfive() {
+    alert("Project 5 Coming Soon!");
+}
+function buttonsix() {
+    alert("Project 6 Coming Soon!");
+}
+function buttonseven() {
+    alert("Project 7 Coming Soon!");
+}
+function buttoneight() {
+    alert("Project 8 Coming Soon!");
+}
+function buttonnine() {
+    alert("Project 9 Coming Soon!");
+}
